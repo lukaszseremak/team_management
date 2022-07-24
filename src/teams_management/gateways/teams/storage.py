98aa66fcccae6ddefef640265f76cb2646695323
lukaszseremak <lukasz.dominik.seremak@gmail.com>
2022-07-24 13:03:04 +0200

    refactor: remove unused comments

--- src/teams_management/gateways/teams/storage.py
+++ src/teams_management/gateways/teams/storage.py
@@ -14,7 +14,7 @@ class DatabaseTeamsStorage(gateways.ITeamsStorage):
         return entities.Team(name=obj.name, uid=obj.uid)
 
     async def create(self, name: str):
-        async with self._storage_session.begin_db_session() as db_session:  # type: ignore
+        async with self._storage_session.begin_db_session() as db_session:
             obj = tables.Team(name=name)
             db_session.add(obj)
             db_session.commit()
@@ -23,7 +23,7 @@ class DatabaseTeamsStorage(gateways.ITeamsStorage):
             return self._map_orm_to_entity(obj)
 
     async def delete(self, uid: UUID):
-        async with self._storage_session.begin_db_session() as db_session:  # type: ignore
+        async with self._storage_session.begin_db_session() as db_session:
             affected_rows = (
                 db_session.query(tables.Team)
                 .filter(
@@ -38,13 +38,13 @@ class DatabaseTeamsStorage(gateways.ITeamsStorage):
             return True
 
     async def fetch_all(self) -> List[entities.Team]:
-        async with self._storage_session.begin_db_session() as db_session:  # type: ignore
+        async with self._storage_session.begin_db_session() as db_session:
             teams = db_session.query(tables.Team).all()
 
             return [self._map_orm_to_entity(team) for team in teams]
 
     async def edit(self, uid: UUID, name: str):
-        async with self._storage_session.begin_db_session() as db_session:  # type: ignore
+        async with self._storage_session.begin_db_session() as db_session:
             team = db_session.query(tables.Team).get(uid)
             if not team:
                 raise gateways.NotFoundError("Team not found")
